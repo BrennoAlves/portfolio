7f4c213f69518e097ca29e120c7beed69aac05fc
BrennoAves <brenno1231@outlook.com>
2024-04-03 15:53:28 -0300

    Atualizando configurações

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+//  
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://brennoalves.vercel.app',
+  integrations: [tailwind(), vercel()], 
   output: 'static',
   adapter: vercel(),
 });

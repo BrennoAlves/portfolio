11d82bc459768239a2c39667f9985621d2066cdd
BrennoAlves <brenno1231@outlook.com>
2025-08-28 08:59:24 -0300

    fix: usar adapter estático para deploy no Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'; 
+import vercel from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
+  output: 'static',
+  adapter: vercel(),
 });
